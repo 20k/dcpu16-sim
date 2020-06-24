4a8a255d28ce38d506cf8e8ff8fec1fa9dcdd059
James <Icedshot@gmail.com>
2020-06-24 21:13:07 +0100

    send/receive time

--- base_sim.hpp
+++ base_sim.hpp
@@ -168,7 +168,7 @@ uint16_t get_cycle_time_instr(uint16_t instr)
         time += get_cycle_time_arg(a);
         time += get_cycle_time_arg(b);
 
-        std::array<uint16_t, 32> times = {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 3, 3, 0, 0, 2, 2};
+        std::array<uint16_t, 32> times = {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 3, 3, 1, 1, 2, 2};
 
         time += times[o];
     }
